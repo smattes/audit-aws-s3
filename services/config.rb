f158c2232c0518239787cdb7c95d7f2759d9b528
George Moberly <georgemoberly@Georges-MacBook-Air-2.local>
2016-12-22 14:30:52 -0800

    make json notifier :nothing

--- services/config.rb
+++ services/config.rb
@@ -217,7 +217,7 @@ end
   HTML SEND METHOD
 =end
 coreo_uni_util_notify "advise-s3-json" do
-  action :notify
+  action :nothing
   type 'email'
   allow_empty ${AUDIT_AWS_S3_ALLOW_EMPTY}
   send_on '${AUDIT_AWS_S3_SEND_ON}'
