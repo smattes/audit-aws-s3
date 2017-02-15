284604faeaa68e78700871446d3d26e87b4a90de
Mikhail <123prove123@gmail.com>
2017-02-15 13:14:25 +0600

    Update jsrunner
    Update jsrunner

--- services/config.rb
+++ services/config.rb
@@ -250,7 +250,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-s3" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.8.2"
+                   :version => "1.8.3"
                },
                {
                    :name => "js-yaml",
