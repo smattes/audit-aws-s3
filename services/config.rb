c5cfc3a871c9f52a7a4cc97e5db419e17899ee0b
Michael Pautov <michael.pautov@gmail.com>
2017-05-15 11:44:30 +0600

    Rewrote input for alert list

--- services/config.rb
+++ services/config.rb
@@ -273,7 +273,7 @@ const OWNER_TAG = "${AUDIT_AWS_S3_OWNER_TAG}";
 const ALLOW_EMPTY = "${AUDIT_AWS_S3_ALLOW_EMPTY}";
 const SEND_ON = "${AUDIT_AWS_S3_SEND_ON}";
 
-const alertListJSON = "[${AUDIT_AWS_S3_ALERT_LIST}]";
+const alertListJSON = [${AUDIT_AWS_S3_ALERT_LIST}];
 const alertListArray = alertListJSON.replace(/'/g, '"');
 const ruleInputs = {};
 
