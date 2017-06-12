8bf0af5ad04a487ccb49d5a2eea590b73517faeb
nursultan <nursultan@cloudcoure.com>
2017-06-12 15:12:09 +0600

    message

--- services/config.rb
+++ services/config.rb
@@ -320,6 +320,7 @@ function setSuppression() {
 }
 
 function setTable() {
+
   userSchemes = yaml.safeLoad(fs.readFileSync('./table.yaml', 'utf8'));
   coreoExport('table', JSON.stringify(userSchemes));
 }
