db3ea35ae35868336bdfa7ca3b8a0f5bef822327
omurbek.jk@gmail.com <omurbek.jk@gmail.com>
2017-08-21 09:00:21 +0600

    CON-626 fix problems with fixed s3 notifier buckets

--- services/config.rb
+++ services/config.rb
@@ -518,10 +518,9 @@ coreo_aws_s3_policy "cloudcoreo-audit-aws-s3-policy" do
   EOF
 end
 
-coreo_aws_s3_bucket "${AUDIT_AWS_S3_S3_NOTIFICATION_BUCKET_NAME}" do
+coreo_aws_s3_bucket "bucket-${AUDIT_AWS_S3_S3_NOTIFICATION_BUCKET_NAME}" do
   action((("${AUDIT_AWS_S3_S3_NOTIFICATION_BUCKET_NAME}".length > 0) ) ? :create : :nothing)
   bucket_policies ["cloudcoreo-audit-aws-s3-policy"]
-  region "us-east-1"
 end
 
 coreo_uni_util_notify "cloudcoreo-audit-aws-s3-s3" do
